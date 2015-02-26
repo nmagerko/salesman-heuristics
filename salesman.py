4155deaa4beac021860c36bced130de3ed2ecf4c
ehennenfent <ehennenfent@imsa.edu>
2015-02-26 04:01:20 -0600

    Minimum distance from edge
    Major:
    Rather than calculating distances to edges based off of the distance to
    the midpoint, we now calculate the minimum distance from any point on
    the edge. See utils.find_shortest_distance().
    
    Minor:
    Added command line parameter for number of cities to visit
    Removed linter-breaking semicolons

--- salesman.py
+++ salesman.py
@@ -4,9 +4,14 @@ import networkx as nx
 import utils
 from matplotlib.cbook import Null
 import math
+import sys
 
 # the number of cities on our itinerary
-CITIES = 40
+if len(sys.argv) > 1:
+    CITIES = int(sys.argv[1])
+else:
+    CITIES = 40
+
 # the number of alternative nearest neighbors to check
 ALTERNATIVES = 3
 # the number of nodes we want to work ahead
@@ -19,9 +24,9 @@ visited_cities = []
 
 visited_edges = []
 
-city_positions = cities.get_city_positions_safely();
+city_positions = cities.get_city_positions_safely()
 
-last_edge = None;
+last_edge = None
 
 def clean_edge_list(edge_list, extra=[]):
     """
@@ -30,7 +35,7 @@ def clean_edge_list(edge_list, extra=[]):
     """
     return [edge for edge in edge_list if edge[1] not in visited_cities \
             and edge[1] not in extra]
-    
+
 
 def get_nearest_neighbor_edges(node, requested=1, traveled=[]):
     """
@@ -43,18 +48,18 @@ def get_nearest_neighbor_edges(node, requested=1, traveled=[]):
     sorted_edges = clean_edge_list(sorted_edges, extra=traveled)
 
     return (sorted_edges[0:requested], sorted_edges[requested:])
-    
-    
+
+
 def determine_best_neighbor_edge(potential_edges):
     neighbor_prediction_weights = []
     for edge in potential_edges:
         outcome_path = []
         outcome_nodes = []
-        
+
         outcome_path.append(edge)
         potential_neighbor = edge[1]
         outcome_nodes.append(potential_neighbor)
-        
+
         for predictions in range(0, PREDICTION_CAP):
             nearest_edge_list = get_nearest_neighbor_edges(node=outcome_nodes[-1], traveled=outcome_nodes)
             if nearest_edge_list[0]:
@@ -62,13 +67,13 @@ def determine_best_neighbor_edge(potential_edges):
                 outcome_path.append(next_edge)
                 next_node = next_edge[1]
                 outcome_nodes.append(next_node)
-            
+
         neighbor_prediction_weights.append({ 'edge': edge,
                                              'weight': utils.total_weight(outcome_path) })
-        
+
     sorted_weights = sorted(neighbor_prediction_weights, key=lambda edge: edge['weight'])
     sorted_weights = [prediction_weight['edge'] for prediction_weight in sorted_weights]
-    
+
     return (sorted_weights[0], sorted_weights[1:])
 
 def draw_solution():
@@ -93,13 +98,13 @@ def solve_salesman_problem():
         visited_cities.append(current_city)
         nearest_edges, rmv = get_nearest_neighbor_edges(current_city, ALTERNATIVES)
         graph.remove_edges_from(rmv)
-        
+
         best_edge, rmv = determine_best_neighbor_edge(nearest_edges)
         graph.remove_edges_from(rmv)
 
         current_city = best_edge[1]
         print("NEXT CITY: " + current_city)
-        
+
     for node in graph.nodes():
         initial_city = visited_cities[0]
         if graph.degree(node) == 1  and node != initial_city:
@@ -109,23 +114,23 @@ def solve_salesman_problem():
             break
 
     draw_solution()
-    
-def findAngle(node1, node2):   
-    initialPosition = city_positions[node1];
-    nextPosition = city_positions[node2];
-    delta_x = nextPosition[0] - initialPosition[0];
-    delta_y = nextPosition[1] - initialPosition[1];
-    quadrant = 1;
+
+def findAngle(node1, node2):
+    initialPosition = city_positions[node1]
+    nextPosition = city_positions[node2]
+    delta_x = nextPosition[0] - initialPosition[0]
+    delta_y = nextPosition[1] - initialPosition[1]
+    quadrant = 1
     if delta_x < 0:
         quadrant = 2
         if delta_y < 0:
-            quadrant = 3;
+            quadrant = 3
     elif delta_y < 0 :
-        quadrant = 4;
-    theta = math.degrees(math.atan(delta_y/delta_x));
+        quadrant = 4
+    theta = math.degrees(math.atan(delta_y/delta_x))
     if quadrant== 2 or quadrant == 3:
-        theta +=180;
-    return theta;
+        theta +=180
+    return theta
 
 def determine_best_radial_neighbor(node):
     """
@@ -133,52 +138,53 @@ def determine_best_radial_neighbor(node):
     @author: jreynolds
     """
     global last_edge
-    nearest_edges = graph.edges(node);
-    angle = 180.0;
+    nearest_edges = graph.edges(node)
+    angle = 180.0
     if last_edge is not None:
-        angle=findAngle(last_edge[0],last_edge[1]);
-    next_edge = None;
-    smallest_angle = None;
+        angle=findAngle(last_edge[0],last_edge[1])
+    next_edge = None
+    smallest_angle = None
     for edge in nearest_edges:
-        possible_next_angle = findAngle(node,edge[1]);  
+        possible_next_angle = findAngle(node,edge[1])
         if possible_next_angle < angle:
-            possible_next_angle += 360;
+            possible_next_angle += 360
         if smallest_angle is None or possible_next_angle < smallest_angle:
-            smallest_angle = possible_next_angle;
-            next_edge = edge;
-    last_edge=next_edge;
+            smallest_angle = possible_next_angle
+            next_edge = edge
+    last_edge=next_edge
     return (next_edge, [rmv for rmv in nearest_edges if rmv != next_edge \
-                                                    and rmv[1] not in visited_cities]);
+                                                    and rmv[1] not in visited_cities])
 
 def find_extreme_vertex():
     """
     Find the most extreme vertex in the -x direction
     @author: jreynolds
     """
-    furthest_node = None;
-    furthest_x= None;
+    furthest_node = None
+    furthest_x= None
     for node in graph.nodes():
         position = city_positions[node]
         if furthest_x is None or position[0] < furthest_x:
-            furthest_x = position[0];
-            furthest_node = node;
-    return furthest_node;
+            furthest_x = position[0]
+            furthest_node = node
+    return furthest_node
 def average_position(node1, node2):
     position1 = city_positions[node1]
     position2 = city_positions[node2]
-    new_position = ((position1[0]+position2[0])/2,(position1[1]+position2[1])/2);
-    return new_position;
-    
+    new_position = ((position1[0]+position2[0])/2,(position1[1]+position2[1])/2)
+    return new_position
+
 def find_closest_outer_edge(inner_node):
-    closest_edge = None;
-    shortest_dist = None;
+    closest_edge = None
+    shortest_dist = None
     for edge in visited_edges:
         edge_center_position = average_position(edge[0],edge[1])
-        distance_to_edge_center = utils.distance(city_positions[inner_node], edge_center_position);
+        # distance_to_edge_center = utils.distance(city_positions[inner_node], edge_center_position)
+        distance_to_edge_center = utils.find_shortest_distance(edge, city_positions[inner_node])
         if shortest_dist is None or distance_to_edge_center< shortest_dist:
             shortest_dist = distance_to_edge_center
-            closest_edge = edge;
-    return closest_edge;
+            closest_edge = edge
+    return closest_edge
 
 def solve_salesman_radially():
     """
@@ -187,27 +193,27 @@ def solve_salesman_radially():
     """
     global visited_edges
     # select the first node
-    current_city = find_extreme_vertex();
+    current_city = find_extreme_vertex()
     print("INITIAL CITY: " + current_city)
-    initial_city_nearest_edges = None;
-    
+    initial_city_nearest_edges = None
+
     # after [CITIES - 1], there's only one edge left to connect
     while len(visited_cities) == 0 or current_city!=visited_cities[0]:
         # get the possible edges to take, and the ones we can remove
         visited_cities.append(current_city)
-        
-        best_edge, rmv = determine_best_radial_neighbor(current_city);
+
+        best_edge, rmv = determine_best_radial_neighbor(current_city)
         if len(visited_cities) > 1:
             graph.remove_edges_from(rmv)
         else:
-            initial_city_nearest_edges=rmv;
+            initial_city_nearest_edges=rmv
 
         current_city = best_edge[1]
         visited_edges.append(best_edge)
         print("NEXT CITY: " + current_city)
     graph.remove_edges_from([rmv for rmv in initial_city_nearest_edges if  rmv[1] != visited_cities[1] \
     and rmv[1]!= visited_cities[len(visited_cities)-1]])
-    
+
     for inner_node in [node for node in graph.nodes() if node not in visited_cities]:
         u, v = find_closest_outer_edge(inner_node)
         print(u, v)
@@ -218,8 +224,8 @@ def solve_salesman_radially():
         graph.add_edge(inner_node, v)
         visited_edges.append((inner_node, u))
         visited_edges.append((inner_node, v))
-    
-    
+
+
 #     for node in graph.nodes():
 #         initial_city = visited_cities[0]
 #         if graph.degree(node) == 1  and node != initial_city:
@@ -229,5 +235,5 @@ def solve_salesman_radially():
 #             break
 
     draw_solution()
-solve_salesman_radially();    
+solve_salesman_radially()
 #solve_salesman_problem()
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import math
 import random
+import cities
 
 def distance(node1, node2):
     return math.sqrt((node1[0] - node2[0])**2 + (node1[1] - node2[1])**2)
@@ -21,5 +22,26 @@ def find_shortest_edge(graph):
     shortest_edge = None
     for edge in graph.edges():
         if shortest_edge is None or edge['weight'] < shortest_edge['weight']:
-            shortest_edge=edge;
-    return shortest_edge;
+            shortest_edge=edge
+    return shortest_edge
+
+def find_shortest_distance(edge, node):
+    city_positions = cities.get_city_positions_safely()
+    C1, C2 = city_positions[edge[0]], city_positions[edge[1]] # edge endpoints
+    x1, y1 = C1[0], C1[1] # endpoint coords
+    x2, y2 = C2[0], C2[1]
+    x0, y0 = node[0], node[1] # point not on edge
+
+    # mathworld.wolfram.com/Point-LineDistance2-Dimensional.html
+    numerator = abs( (x2-x1)*(y1-y0) - (x1-x0)*(y2-y1) )
+    denominator = math.sqrt( (y2 - y1)**2 + (x2 - x1)**2 )
+
+    mag = ( ( (x0-x1) * (x2-x1) ) + ( (y0-y1) * (y2-y1) ) )
+    if(mag < 0) or (mag  > (x2-x1)**2+(y2-y1)**2): # Checks if closest point
+                                                   # not on segment
+        dist1 = distance(C1,node)
+        dist2 = distance(C2,node)
+        return min(dist2,dist1) # returns distance to closer endpoint
+    else:
+        return (numerator/denominator) # returns distance to closest point
+                                       # between endpoints
